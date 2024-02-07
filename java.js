98c7cd40cc8277057f30e91ece88ec893294872c
Arpita karki <151529261+Arpitakarki@users.noreply.github.com>
2024-02-07 22:49:31 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,158 @@
+let a = 20;
+let b = 20;
+
+document.getElementById('first').innerHTML = (a = b);
+
+let c = 30;
+let d = 40;
+
+document.getElementById('f2').innerHTML = (c += d);
+
+let e = 10;
+let f = 15;
+
+document.getElementById('f3').innerHTML = (e -= f);
+
+let g = 25;
+let h = 35;
+
+document.getElementById('f4').innerHTML = (g *= h);
+
+let i = 45;
+let j = 50;
+
+document.getElementById('f5').innerHTML = (i /= j)
+
+let k = 65;
+let l = 55;
+
+document.getElementById('f6').innerHTML = (k %= l);
+
+let m = 60;
+let n = 70;
+
+document.getElementById('f7').innerHTML = (m **= n);
+
+let ab = 10;
+let ba = 20;
+
+document.getElementById('f8').innerHTML = (ab == ba);
+
+let cd = 30;
+let dc = 40;
+
+document.getElementById('f9').innerHTML = (cd === dc);
+
+let ef = 50;
+let fe = 60;
+
+document.getElementById('f10').innerHTML = (ef != fe);
+
+let gh = 70;
+let hg = 80;
+
+document.getElementById('f11').innerHTML = (gh !== hg);
+
+let ij = 90;
+let ji = 100;
+
+document.getElementById('f12').innerHTML = (ij > ji);
+
+let kl = 15;
+let lk = 25;
+
+document.getElementById('f13').innerHTML = (kl < lk);
+
+let mn = 35;
+let nm = 45;
+
+document.getElementById('f14').innerHTML = (mn >= nm);
+
+let op = 55;
+let po = 65;
+
+document.getElementById('f15').innerHTML = (op <= nm);
+
+
+
+///function click
+
+function myFun() {
+    let java = document.getElementById("arpita")
+    if (java.style.display ==="none"){
+        java.style.display ="block";
+    }
+    else{
+        java.style.display ="none";
+    }
+}
+
+function scRol(){
+    let name = document.getElementById("a1")
+    if(name.style.display ==="none"){
+        name.style.display ="block";
+    }
+    else{
+        name.style.display ="none";
+    }
+}
+
+function scRoll(){
+    let name = document.getElementById("a1")
+    name.style.display ="none"
+    let name2 = document.getElementById("a2")
+    if(name2.style.display ==="none"){
+        name2.style.display ="block";
+    }
+    else{
+        name2.style.display ="none";
+    }
+}
+
+function scRol1(){
+    let name = document.getElementById("a1")
+    name.style.display ="none"
+    let name2 = document.getElementById("a2")
+    name2.style.display ="none"
+    let name3 = document.getElementById("a3")
+    if(name3.style.display ==="none"){
+        name3.style.display ="block";
+    }
+    else{
+        name3.style.display ="none";
+    }
+}
+
+function scRol2(){
+    let name = document.getElementById("a1")
+    name.style.display ="none"
+    let name2 = document.getElementById("a2")
+    name2.style.display ="none"
+    let name3 = document.getElementById("a3")
+    name3.style.display ="none"
+    let name4 = document.getElementById("a4")
+    if(name4.style.display ==="none"){
+        name4.style.display ="block";
+    }
+    else{
+        name4.style.display ="none";
+    }
+}
+
+function scRol3(){
+    let name = document.getElementById("a1")
+    name.style.display ="none"
+    let name2 = document.getElementById("a2")
+    name2.style.display ="none"
+    let name3 = document.getElementById("a3")
+    name3.style.display ="none"
+    let name4 = document.getElementById("a4")
+    name4.style.display ="none"
+    let name5 = document.getElementById("a5")
+    if(name5.style.display ==="none"){
+        name5.style.display ="block";
+    }
+    else{
+        name5.style.display ="none";
+    }
+}
